b4b47699d69ed8b1eb1ca23637e0f5290d326a12
JohannC <johann.corcuff@gmail.com>
2019-04-01 01:43:42 +0200

    Demo Evolutions

--- src/App.js
+++ src/App.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import logo from "./img/logo.svg";
 import VideoPlayerCodeSnippet from './component/VideoPlayerCodeSnippet';
+import AudioPlayerCodeSnippet from './component/AudioPlayerCodeSnippet';
+import SlideshowPlayerCodeSnippet from './component/SlideshowPlayerCodeSnippet';
+import AudioVinylPlayerCodeSnippet from './component/AudioVinylPlayerCodeSnippet';
 import ReactWebMediaPlayer from 'react-web-media-player';
+//import heart from './img/heart.svg';
 
 class App extends Component {
   render() {
     return (
-      <div classNameName="App">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-          <a class="logo-navbar navbar-brand ml-5" href="http://futurelink.com">
-            <img src={logo} height="35" />
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-dark">
+          <a className="logo-navbar navbar-brand ml-5" href="http://futurelink.com">
+            <img src={logo} height="35" alt=""/>
             <span className="ml-3">React Web Media Player</span>
           </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
               </li>
-              <li class="nav-item">
-                <a class="nav-link ml-5" href="#">Docs</a>
+              <li className="nav-item">
+                <a className="nav-link ml-5" href="#">Docs</a>
               </li>
             </ul>
           </div>
@@ -32,7 +36,7 @@ class App extends Component {
           <div className="container">
             <div className="row">
               <div className="col-xl-9 mx-auto">
-                <img src={logo} className="spin mb-4" height={(window.innerWidth > 768) ? "300px" : "150px"} />
+                <img src={logo} className="spin mb-4" height={(window.innerWidth > 768) ? "300px" : "150px" } alt=""/>
                 <h1 className="logo-title">React Web Media Player</h1>
                 <p className="logo-subtitle">Your custom video, audio and slideshow player!</p>
               </div>
@@ -65,7 +69,7 @@ class App extends Component {
                 <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                   <div className="features-icons-icon d-flex">
                     <i className="fas m-auto feature-icons-color my-svg">
-                      <svg aria-hidden="true" class="my-svg" focusable="false" data-prefix="far" data-icon="presentation" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-presentation fa-w-18 fa-7x"><path fill="#61dafb" d="M560 0H16C7.16 0 0 7.16 0 16v16c0 8.84 7.16 16 16 16h16v272c0 17.67 14.33 32 32 32h200v43.72l-77.65 77.65c-6.25 6.25-6.25 16.38 0 22.63l11.31 11.31c6.25 6.25 16.38 6.25 22.63 0L288 439.6l67.72 67.72c6.25 6.25 16.38 6.25 22.63 0l11.3-11.32c6.25-6.25 6.25-16.38 0-22.63L312 395.72V352h200c17.67 0 32-14.33 32-32V48h16c8.84 0 16-7.16 16-16V16c0-8.84-7.16-16-16-16zm-64 304H80V48h416v256z" class=""></path></svg>
+                      <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="presentation" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="my-svg svg-inline--fa fa-presentation fa-w-18 fa-7x"><path fill="#61dafb" d="M560 0H16C7.16 0 0 7.16 0 16v16c0 8.84 7.16 16 16 16h16v272c0 17.67 14.33 32 32 32h200v43.72l-77.65 77.65c-6.25 6.25-6.25 16.38 0 22.63l11.31 11.31c6.25 6.25 16.38 6.25 22.63 0L288 439.6l67.72 67.72c6.25 6.25 16.38 6.25 22.63 0l11.3-11.32c6.25-6.25 6.25-16.38 0-22.63L312 395.72V352h200c17.67 0 32-14.33 32-32V48h16c8.84 0 16-7.16 16-16V16c0-8.84-7.16-16-16-16zm-64 304H80V48h416v256z"></path></svg>
                     </i>
                   </div>
                   <h3>Play slideshows</h3>
@@ -115,7 +119,7 @@ class App extends Component {
                   thumbnail="https://i.ytimg.com/vi/aqz-KE-bpKQ/maxresdefault.jpg"
                   video="https://nusid.net/video.mp4"
                   logo={{ img: "https://nusid.net/your-logo.png", src: "https://www.npmjs.com/package/react-web-media-player" }}
-                  style={{ margin: "auto", marginTop: "70px" }}
+                  style={{ margin: "auto", marginTop: "0px" }}
                 />
               </div>
               <div className="col-lg-6 order-lg-1 my-auto showcase-text">
@@ -128,7 +132,6 @@ class App extends Component {
                 <ReactWebMediaPlayer
                   width="400" height="400"
                   title="Key Notez - Summer"
-                  link="https://google.com"
                   thumbnail={"https://nusid.net/album-cover.jpg"}
                   audio="https://nusid.net/audio.mp3"
                   logo={{ img: "https://nusid.net/your-logo.png", src: "https://www.npmjs.com/package/react-web-media-player" }}
@@ -137,14 +140,30 @@ class App extends Component {
               </div>
               <div className="col-lg-6 order-lg-1 my-auto showcase-text">
                 <h2 className="mb-4">Audio player</h2>
-                <VideoPlayerCodeSnippet />
+                <AudioPlayerCodeSnippet />
+              </div>
+            </div>
+            <div className="row no-gutters">
+              <div className="col-lg-6 order-lg-2 text-white showcase-img pt-5">
+                <ReactWebMediaPlayer
+                  width="400" height="400"
+                  title="Key Notez - Summer"
+                  thumbnail={"https://nusid.net/album-cover.jpg"}
+                  audio="https://nusid.net/audio.mp3"
+                  vinyl={{ img: "https://nusid.net/album-cover.jpg", rpm: 10}}
+                  logo={{ img: "https://nusid.net/your-logo.png", src: "https://www.npmjs.com/package/react-web-media-player" }}
+                  style={{ margin: "auto" }}
+                />
+              </div>
+              <div className="col-lg-6 order-lg-1 my-auto showcase-text">
+                <h2 className="mb-4">Retro vinyl audio player</h2>
+                <AudioVinylPlayerCodeSnippet />
               </div>
             </div>
             <div className="row no-gutters">
               <div className="col-lg-6 order-lg-2 text-white pt-5">
                 <ReactWebMediaPlayer
                   title="My Slideshow Player"
-                  link="https://google.com"
                   thumbnail="https://nusid.net/slide1.jpg"
                   slideshow={
                     [{
@@ -170,41 +189,12 @@ class App extends Component {
                       endTime: 28.0
                     }]}
                   logo={{ img: "https://nusid.net/your-logo.png", src: "https://www.npmjs.com/package/react-web-media-player" }}
-                  style={{ margin: "auto", marginTop: "70px" }}
+                  style={{ margin: "auto", marginTop: "80px", marginBottom:"30px"}}
                 />
               </div>
               <div className="col-lg-6 order-lg-1 my-auto showcase-text">
                 <h2 className="mb-4">Slideshow player</h2>
-                <VideoPlayerCodeSnippet />
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className="testimonials text-center bg-light">
-          <div className="container">
-            <h2 className="mb-5">What people are saying...</h2>
-            <div className="row">
-              <div className="col-lg-4">
-                <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                  <img className="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="" />
-                  <h5>Margaret E.</h5>
-                  <p className="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                  <img className="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="" />
-                  <h5>Fred S.</h5>
-                  <p className="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                  <img className="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="" />
-                  <h5>Sarah W.</h5>
-                  <p className="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
-                </div>
+                <SlideshowPlayerCodeSnippet />
               </div>
             </div>
           </div>
--- src/component/SlideshowSoundtrackPlayerCodeSnippet.js
+++ src/component/SlideshowSoundtrackPlayerCodeSnippet.js
@@ -3,9 +3,9 @@ import { defineCustomElements } from 'snippet-highlight/dist/loader';
 defineCustomElements(window);
 
 
-const VideoPlayerCodeSnippet = () => {
+const SlideshowSoundtrackPlayerCodeSnippet = () => {
   const codeSnippet = '<ReactWebMediaPlayer\n\ttitle="Big Buck Bunny - 2008"\n\tthumbnail="https://i.ytimg.com/vi/aqz-KE-bpKQ/maxresdefault.jpg"\n\tvideo="https://nusid.net/video.mp4"\n\tlogo={{\n\t\timg: "https://nusid.net/your-logo.png", \n\t\tsrc:"https://www.npmjs.com/package/react-web-media-player"\n\t}}\n/>';
   return <snippet-highlight theme="dark" language="jsx" content={codeSnippet}/>
 }
 
-export default VideoPlayerCodeSnippet;
+export default SlideshowSoundtrackPlayerCodeSnippet;
