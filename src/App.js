69548cb3f4890a4126a07dad8b80aa48154c218d
JohannC <johann.corcuff@gmail.com>
2019-04-04 23:49:49 +0200

    Miscellaneous improvements

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import NavBar from './component/partial/NavBar';
 import Footer from './component/partial/Footer';
 import Homepage from './component/page/homepage/Homepage';
 import Docs from './component/page/docs/Docs';
-import {Route, Switch } from 'react-router-dom';
-import loadjs from 'loadjs';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  componentWillMount() {
-    /*loadjs('../public/vendor/jquery/jquery.min.js');
-    loadjs('../public/vendor/bootstrap/js/bootstrap.bundle.min.js');
-    loadjs('css!/../public/vendor/bootstrap/css/bootstrap.min.css');*/
-  }
   render() {
     return (
       <div className="App">
+        <Router>
           <NavBar />
           <Switch>
-          <Route path="/Demo-React-Web-Media-Player/docs" component={Docs} />
-
+            <Route path="/Demo-React-Web-Media-Player/docs" component={Docs} />
             <Route component={Homepage} />
           </Switch>
           <Footer />
+        </Router>
       </div>
     );
   }
--- src/component/page/docs/Docs.js
+++ src/component/page/docs/Docs.js
@@ -7,8 +7,11 @@ class Docs extends Component {
             <div className="page">
                 <div class="container">
                     <h1 className="mt-5">Docs</h1>
-                    <p className="mb-5 mt-5" style={{fontSize:"18px"}}>This page is under active construction! Coming soon :)</p>
-                    <img className="mb-5" style={{textAlign:"center"}} src={comingSoonImg} alt="" />
+                    <p className="mb-5 mt-5" style={{ fontSize: "18px" }}>
+                        This page is under active construction! Will be released around mid april 2019 :)
+                        
+                    </p>
+                    <img className="img-fluid rounded mb-5" src={comingSoonImg} alt="" />
                 </div>
             </div>
         );
