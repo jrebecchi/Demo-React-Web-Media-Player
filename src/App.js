c9780c5e2f4dda709b6ae8bea908830da8d4e38d
JohannC <johann.corcuff@gmail.com>
2019-04-04 23:36:58 +0200

    Bug fix - meta js script or css stylesheet not loading if entring the app not through its index page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import NavBar from './component/partial/NavBar';
 import Footer from './component/partial/Footer';
 import Homepage from './component/page/homepage/Homepage';
 import Docs from './component/page/docs/Docs';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {Route, Switch } from 'react-router-dom';
+import loadjs from 'loadjs';
 
 class App extends Component {
+  componentWillMount() {
+    /*loadjs('../public/vendor/jquery/jquery.min.js');
+    loadjs('../public/vendor/bootstrap/js/bootstrap.bundle.min.js');
+    loadjs('css!/../public/vendor/bootstrap/css/bootstrap.min.css');*/
+  }
   render() {
     return (
       <div className="App">
-        <Router>
           <NavBar />
-          <Route exact path="/Demo-React-Web-Media-Player" component={Homepage} />
+          <Switch>
           <Route path="/Demo-React-Web-Media-Player/docs" component={Docs} />
+
+            <Route component={Homepage} />
+          </Switch>
           <Footer />
-        </Router>
       </div>
     );
   }
