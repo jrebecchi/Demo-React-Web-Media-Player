7e7d9c542a091857f94280a2573bec8e7402a527
JohannC <johann.corcuff@gmail.com>
2019-04-05 00:39:31 +0200

    Routing bug fixed

--- src/component/page/homepage/partial/Banner.js
+++ src/component/page/homepage/partial/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from "../../../../img/logo.svg";
+import triangle from "../../../../img/triangle.svg";
 
 const Banner = () => {
     return (
@@ -9,6 +10,8 @@ const Banner = () => {
                 <div className="row">
                     <div className="col-xl-9 mx-auto">
                         <img src={logo} className="spin mb-4" height={(window.innerWidth > 768) ? "300px" : "150px"} alt="" />
+                        {/*<img src={triangle} style={{position: "absolute", left:"283px"}}height={(window.innerWidth > 768) ? "300px" : "150px"} alt="" />*/}
+
                         <h1 className="logo-title">React Web Media Player</h1>
                         <p className="logo-subtitle">Your custom video, audio and slideshow player!</p>
                     </div>
